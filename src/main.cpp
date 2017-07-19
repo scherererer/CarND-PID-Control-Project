5ba3b2d1e0f51a3f85795f64c57df2af77c504f7
Michael P. Scherer <scherer@protonmail.ch>
2017-07-18 21:58:28 -0500

    Pretty good looking values

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main()
 
   PID pid;
 
-  pid.Init (0.4, 0.0, 0.3);
+  pid.Init (0.4, 0.0, 0.35);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
